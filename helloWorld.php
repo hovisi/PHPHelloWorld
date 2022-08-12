cf82e7c3c6b6aa41c69dd12595e92561fef2cb08
hovisi <51930383+hovisi@users.noreply.github.com>
2022-08-11 23:00:49 -0400

    Add files via upload

---
+++ helloWorld.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    Bella Hovis  
+    CSCI 297 Scripting Languages   
+    Assignment: Hello World  
+    Description: Program uses basic HTML and structure and output Hello World using an echo statement from PHP mode. 
+*/
+?> 
+
+<html>
+   
+
+    <body>
+        <h1> Bella Hovis Hello World</h1>
+        
+        <?php // enter PHP mode
+            echo "Hello World "; 
+        ?> 
+
+    </body>
+
+</html>
